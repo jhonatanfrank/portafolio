d705a1230acd64a6724f9aaa90c0ea3bdc4e8592
jhonatanfrank <frank_mch97@hotmail.com>
2022-08-23 17:47:57 -0500

    cambiar navbar 2

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = ({ isScrolling }) => {
 
   return (
     <nav className={`navbar ${isScrolling > 20 ? "scrolling" : null}`}>
-      <div className="navbar-logo" onClick={toTheTop}>Frank Jhonatan</div>
+      <div className="navbar-logo" onClick={toTheTop}>Mi Portafolio</div>
     </nav>
   );
 };
