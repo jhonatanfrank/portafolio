eff1fa425c010d3249702d6be6f76fe95526ecd3
jhonatanfrank <frank_mch97@hotmail.com>
2022-08-23 19:52:22 -0500

    modificacion en titulo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
               <h3>Contáctame</h3>
               <p>Comunicarse conmigo para cualquier trabajo</p>
             </div>
-            <div className="design-by">Realizado por Frank Jhonatan Chancafe Miramira</div>
+            <div className="design-by">Realizado por: Frank Jhonatan Chancafe Miramira</div>
             <div className="sns-links">
               <a href="https://linkedin.com" target="_blank" rel="noreferrer">
                 <i className="fab fa-linkedin linkedin"></i>
@@ -27,23 +27,31 @@ const Footer = () => {
               </a>
             </div>
           </div>
-          <div class="col-sm-6 texto"><h1>Llena este formulario: </h1>
-          <hr></hr>
-            <div class="mb-3">
-              <label for="nombre" class="form-label texto">Nombre:</label>
-              <input type="email" class="form-control" id="nombre" placeholder="Ingrese su nombre" />
-            </div>
-            <div class="mb-3">
-              <label for="correoelectronico" class="form-label texto">Dirección de correo electrónico:</label>
-              <input type="email" class="form-control" id="correoelectronico" placeholder="Ingrese su correo electronico" />
-            </div>
-            <div class="mb-3">
-              <label for="exampleFormControlTextarea1" class="form-label texto">Mensaje:</label>
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div></div>
+          <div class="col-sm-6 texto">
+            <hr></hr>
+            <h1>Llena este formulario: </h1>
+            <form action="https://formspree.io/f/mzbwzekq"
+              method="POST">
+              <div class="mb-3">
+                <label for="nombre" class="form-label texto">Nombre:</label>
+                <input type="text" name="nombre" class="form-control" id="nombre" placeholder="Ingrese su nombre" />
+              </div>
+              <div class="mb-3">
+                <label for="correoelectronico" class="form-label texto">Dirección de correo electrónico:</label>
+                <input type="email" name="correoelectronico" class="form-control" id="correoelectronico" placeholder="Ingrese su correo electronico" />
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label texto">Mensaje:</label>
+                <textarea class="form-control" name="mensaje" id="exampleFormControlTextarea1" rows="3"></textarea>
+              </div>
+              <div class="mb-3">
+                <button class="btn btn-danger" type="submit">Enviar</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
