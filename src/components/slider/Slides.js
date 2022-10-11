4947ee48d111cf9871c59c00ef4630210db520fd
Jhonatan Frank <98656878+jhonatanfrank@users.noreply.github.com>
2022-10-10 23:16:37 -0500

    Update Slides.js

--- src/components/slider/Slides.js
+++ src/components/slider/Slides.js
@@ -9,11 +9,13 @@ const slidesInfo = [
     desc: "Project 1",
   },
   {
+    href: "https://rick-and-morty-jhonatanfrank.vercel.app/",
     src: "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
     alt: "Project 2",
     desc: "Project 2",
   },
   {
+    href: "https://rick-and-morty-jhonatanfrank.vercel.app/",
     src: "https://cdn.pixabay.com/photo/2015/02/05/08/06/macbook-624707_960_720.jpg",
     alt: "Project 3",
     desc: "Project 3",
@@ -22,10 +24,12 @@ const slidesInfo = [
 
 const slides = slidesInfo.map((slide) => (
   <div className="slide-container">
+  <a href={slide.href}>
     <img src={slide.src} alt={slide.alt} />
     <div className="slide-desc">
       <span>{slide.desc}</span>
     </div>
+  </a>
   </div>
 ));
 
