8f20886422c8246005679b579a51853e6b61288a
jhonatanfrank <frank_mhc97@hotmail.com>
2022-10-17 11:00:26 -0500

    actualizando los proyectos

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,27 +1,40 @@
-import React from "react";
-import Carousel from "@brainhubeu/react-carousel";
-import "@brainhubeu/react-carousel/lib/style.css";
-import Slides from "./Slides";
-import "./Slider.css"
+import React from 'react'
+import "../slider/Slider.css"
 
 const Slider = () => {
   return (
-    <div className="carousel-container">
-      <div className="carousel-title">
-        <h2>Mis Proyectos</h2>
+    <div className='contenedor-slider'>
+      <h1 className='titulo-proyecto'>Proyectos:</h1>
+      <div className='contenedor-principal'>
+        <a target="_blank" href="https://rick-and-morty-sand-seven.vercel.app/">
+          <div className='contenedor-item'>
+            <div className='contenedor-nombre'>
+              <h1 className='subtitulo-proyecto'>Rick and Morty</h1>
+            </div>
+            <div className='contenedor-imagen-proyecto'>
+              <img src={require("../slider/rickandmorty.jpg")} className="imagen" />
+            </div>
+            <div className='contenedor-descripcion'>
+              <p className='parrafo-proyecto'>Listado de los personajes de la serie.</p>
+            </div>
+          </div>
+        </a>
+        <a target="_blank" href="https://pokemon-ruddy-ten.vercel.app/">
+          <div className='contenedor-item'>
+            <div className='contenedor-nombre'>
+              <h1 className='subtitulo-proyecto'>Pokemón</h1>
+            </div>
+            <div className='contenedor-imagen'>
+              <img src={require("../slider/pokemon.jpg")} className="imagen" />
+            </div>
+            <div className='contenedor-descripcion'>
+              <p className='parrafo-proyecto'>Listado de los personajes del anime.</p>
+            </div>
+          </div>
+        </a>
       </div>
-      <Carousel
-        arrows
-        slidesPerPage={3}
-        infinite
-        animationSpeed={200}
-        centered
-        offset={50}
-        itemWidth={400}
-        slides={Slides}
-      />
     </div>
-  );
-};
+  )
+}
 
-export default Slider;
+export default Slider
